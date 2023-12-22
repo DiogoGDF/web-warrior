09b0cfcc1fa23f3e98d99dbf868b456867b9718a
Diogo Garbinato <diogogarbinato@gmail.com>
2023-12-22 11:22:12 -0300

    feat: main pages

---
+++ src/pages/about.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function About() {
+  return (
+      <div>
+          <h1>About Page</h1>
+
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent in diam vel sem lacinia fermentum. Ut scelerisque mi et egestas ultrices. Pellentesque volutpat diam et consectetur blandit. Nullam sit amet elit et nunc dictum accumsan nec sit amet neque. Duis a massa ornare, congue risus ac, venenatis sapien. Maecenas congue odio non leo laoreet, vitae laoreet erat dignissim. Nulla sit amet hendrerit leo. Praesent non massa vel augue convallis maximus. In odio nulla, interdum vitae nisi ac, ornare porta mi. Morbi eleifend eget diam id laoreet. Suspendisse viverra, felis sit amet cursus cursus, nibh dolor tempor leo, sit amet porttitor nunc ipsum id massa.</p>
+
+          <p>Nam nec porta sapien. Vestibulum at suscipit odio. Nunc pellentesque imperdiet ipsum, vel faucibus mauris interdum nec. Maecenas metus mi, ornare quis nisi non, egestas iaculis ipsum. In ut tellus tristique, volutpat metus eget, molestie ex. Phasellus vel neque fringilla, bibendum ex id, condimentum orci. Suspendisse sagittis ligula eleifend, accumsan augue vitae, blandit arcu. Cras nec pharetra odio. Cras ex mauris, consequat nec diam et, maximus efficitur augue. Praesent vestibulum quis urna ut faucibus. Nam vel dui eu erat mollis imperdiet non eu dui. Aenean tempus lorem sem, eget tincidunt nibh blandit sit amet. Donec et porta leo, a interdum mauris. Cras ullamcorper libero nec suscipit blandit.</p>
+
+          <p>In fringilla venenatis mattis. Donec sed erat nisi. Aliquam mi sem, auctor nec dui ut, ornare semper augue. Vivamus ex erat, tincidunt non tristique ac, vestibulum ac augue. Integer non auctor enim. Donec lobortis hendrerit elit at bibendum. Proin gravida convallis eleifend. Sed bibendum porta nisi, nec pretium ante eleifend et. Pellentesque et mattis arcu, at ornare justo. Integer aliquet nibh magna, a interdum tellus auctor a. Nulla luctus turpis ac nisl dictum euismod.</p>
+      </div>
+  )
+}
